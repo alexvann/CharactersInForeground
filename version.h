e6e1767781df68f21c100307edb9952f041e07a5
Alexandr <94700659+alexvann@users.noreply.github.com>
2022-09-10 17:44:35 +0300

    Update version.h

--- version.h
+++ version.h
@@ -1,9 +1,9 @@
 #ifndef __VERSION_H
 #define __VERSION_H
 
-#define MOD_NAME			"CharactersInForeground"
+#define MOD_NAME		"CharactersInForeground"
 #define VERSION_MAJOR		1
-#define VERSION_MINOR		3
+#define VERSION_MINOR		4
 #define VERSION_REVISION	0
 
 #endif
